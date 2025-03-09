fb836dbbb67fdbd7573b9192b68d1d9ae01691e8
Gutasss <gutmota23@gmail.com>
2025-03-09 19:13:23 -0300

    exe2-feito

--- exe2/main.c
+++ exe2/main.c
@@ -1,5 +1,6 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
@@ -11,8 +12,27 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile bool timer_tick_flag_r = false;  
+volatile bool timer_tick_flag_g = false;  
+
+volatile bool timer_active_r = false;
+volatile bool timer_active_g = false;
+
+struct repeating_timer timerr;  
+struct repeating_timer timerg;  
+
+bool timer_callback_red(struct repeating_timer *t) {
+    timer_tick_flag_r = true;
+    return true;  
+}
+
+bool timer_callback_green(struct repeating_timer *t) {
+    timer_tick_flag_g = true;
+    return true;  
+}
+
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) {
+    if (events == 0x4) {  
         if (gpio == BTN_PIN_R)
             flag_r = 1;
         else if (gpio == BTN_PIN_G)
@@ -21,32 +41,64 @@ void btn_callback(uint gpio, uint32_t events) {
 }
 
 int main() {
+    stdio_init_all();
+    
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
-
+    gpio_put(LED_PIN_R, 0);
+    
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
-
+    gpio_put(LED_PIN_G, 0);
+    
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-
+    
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
-
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
-
+    
     while (true) {
-
         if (flag_r) {
             flag_r = 0;
+            if (!timer_active_r) {
+                add_repeating_timer_ms(500, timer_callback_red, NULL, &timerr);
+                timer_active_r = true;
+            } else {
+                cancel_repeating_timer(&timerr);
+                timer_active_r = false;
+                gpio_put(LED_PIN_R, 0);
+            }
         }
-
+        
         if (flag_g) {
             flag_g = 0;
+            if (!timer_active_g) {
+                add_repeating_timer_ms(250, timer_callback_green, NULL, &timerg);
+                timer_active_g = true;
+            } else {
+                cancel_repeating_timer(&timerg);
+                timer_active_g = false;
+                gpio_put(LED_PIN_G, 0);
+            }
+        }
+        
+        if (timer_tick_flag_r) {
+            timer_tick_flag_r = false;
+            int state = gpio_get(LED_PIN_R);
+            gpio_put(LED_PIN_R, !state);
+        }
+        
+        if (timer_tick_flag_g) {
+            timer_tick_flag_g = false;
+            int state = gpio_get(LED_PIN_G);
+            gpio_put(LED_PIN_G, !state);
         }
     }
+    
+    return 0;
 }
